814c606663e7a4fdff0e6d12bbbcd0bf93a254ec
OSCAR R <oscr.rss@gmail.com>
2019-02-14 00:22:58 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,26 @@
+def is_prime?(num)
+(2..num/2).none?{|i| num % i == 0}
+   if num < 2
+     return false
+  end
+  i = 2
+  while i <= num / 2
+  if num % i == 0
+  return false
+  end
+  i += 1
+  end
+  return true
+end
+puts is_prime?(4)
+def nth_prime(num)
+  prime_idx = 0
+  count = -1
+  while prime_idx < num
+    count += 1
+    if is_prime?(count) == true
+      prime_idx += 1
+    end
+  end
+  return count
+end
