b79bf47c7ee6a610b572d49b524625e0aafe5503
OSCAR R <oscr.rss@gmail.com>
2019-02-14 00:25:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  (2..num/2).none?{|i| num % i == 0}
+   if num < 2
+     return false
+  end
+  i = 2
+  while i <= num / 2
+  if num % i == 0
+  return false
+  end
+  i += 1
+  end
+  return true
+end
+
